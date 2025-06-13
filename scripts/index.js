3f9fc90629b90d9347ff82eb75a95678ff9dba86
ATimin <Timin.1996@list.ru>
2025-06-13 15:30:09 +0300

    First JS DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const container = document.querySelector(".content");
+const cardsContainer = container.querySelector(".places__list");
 
-// @todo: DOM узлы
+initialCards.forEach((cardData) => {
+  const card = addCard(cardData);
+  cardsContainer.append(card);
+});
 
-// @todo: Функция создания карточки
+function addCard(cardData) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCardItem(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCardItem(cardElement) {
+  cardElement.remove();
+}
